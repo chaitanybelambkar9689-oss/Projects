6c76b205690b9b98a9e08699385b6f25b95be76c
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-05-05 00:17:58 +0530

    Add files via upload

---
+++ Java_projects/MarvellousfullstackApplication.java
@@ -0,0 +1,14 @@
+package com.marvellous.Marvellousfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvellousfullstackApplication {
+
+	public static void main(String[] args)
+    {
+		SpringApplication.run(MarvellousfullstackApplication.class, args);
+	}
+
+}
